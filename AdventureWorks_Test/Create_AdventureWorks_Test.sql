ca5a02e59eebfe067feff9c6cf9207eb1908e012
Brian Thomas <brian.thomas@btechnical-consulting.com>
2022-11-14 19:31:24 -0600

    Added Agents.EconomicAgents, Agents.Person, Agents.ContactType tables

--- AdventureWorks_Test/Create_AdventureWorks_Test.sql
+++ AdventureWorks_Test/Create_AdventureWorks_Test.sql
@@ -97,3 +97,20 @@ GO
 ALTER TABLE [Agents].[Person] CHECK CONSTRAINT [CK_Person_PersonType]
 GO
 
+-- Agents.ContactType
+CREATE TABLE [Agents].[ContactType](
+	[ContactTypeID] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [dbo].[Name] NOT NULL,
+	[ModifiedDate] [datetime] NOT NULL,
+ CONSTRAINT [PK_ContactType_ContactTypeID] PRIMARY KEY CLUSTERED 
+(
+	[ContactTypeID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [Agents].[ContactType] ADD  CONSTRAINT [DF_ContactType_ModifiedDate]  DEFAULT (getdate()) FOR [ModifiedDate]
+GO
+
+
+
