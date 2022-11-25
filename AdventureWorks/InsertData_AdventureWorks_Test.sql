d5323bd815a6606a175dc3515fb15e82d49745c2
brthomasusa <brian.thomas@btechnical-consulting.com>
2022-11-25 16:05:39 -0600

    Clean up of InsertData_AdventureWorks_Test.sql

--- AdventureWorks/InsertData_AdventureWorks_Test.sql
+++ AdventureWorks/InsertData_AdventureWorks_Test.sql
@@ -1,4 +1,4 @@
-USE AdventureWorks_Dev
+USE AdventureWorks_Test
 GO
 -- Used to load the database
 CREATE OR ALTER Proc dbo.usp_InitializeTestDb
@@ -17,81 +17,17 @@ BEGIN
             Delete from Person.EmailAddress;
             Delete from Person.BusinessEntityContact;
             Delete from Person.Person;
-            Delete from Person.StateProvince;
-            Delete from Sales.SalesTerritory;
 
             -- These have no dependencies
             Delete from Person.BusinessEntity;
             Delete from HumanResources.Department;
-            Delete from HumanResources.Shift;            
-            Delete from Person.CountryRegion;
-            Delete from Person.PhoneNumberType; 
-            Delete from Person.ContactType;
-            Delete from Person.AddressType;
-
+            Delete from HumanResources.Shift;           
 
             DBCC CHECKIDENT ("Person.Address", RESEED, 0);                    
             DBCC CHECKIDENT ("Person.EmailAddress", RESEED, 0);
-            DBCC CHECKIDENT ("Person.StateProvince", RESEED, 0);
-            DBCC CHECKIDENT ("Sales.SalesTerritory", RESEED, 0);
             DBCC CHECKIDENT ("Person.BusinessEntity", RESEED, 0);
             DBCC CHECKIDENT ("HumanResources.Department", RESEED, 0);
             DBCC CHECKIDENT ("HumanResources.Shift", RESEED, 0);
-            DBCC CHECKIDENT ("Person.PhoneNumberType", RESEED, 0);
-            DBCC CHECKIDENT ("Person.ContactType", RESEED, 0);
-            DBCC CHECKIDENT ("Person.AddressType", RESEED, 0);
-
-            -- Person.AddressType
-            SET IDENTITY_INSERT Person.AddressType ON;
-                INSERT INTO Person.AddressType
-                    ([AddressTypeID], [Name], [rowguid], [ModifiedDate])
-                SELECT 
-                    [AddressTypeID], [Name], [rowguid], [ModifiedDate]
-                FROM AdventureWorks2019.Person.AddressType;
-            SET IDENTITY_INSERT Agents.AddressType OFF;
-
-            -- Person.ContactType
-            SET IDENTITY_INSERT Person.ContactType ON;
-                INSERT INTO Person.ContactType
-                    ([ContactTypeID], [Name], [ModifiedDate])
-                SELECT 
-                    [ContactTypeID], [Name], [ModifiedDate]
-                FROM AdventureWorks2019.Person.ContactType;
-            SET IDENTITY_INSERT Person.ContactType OFF;
-
-            -- Person.PhoneNumberType
-            SET IDENTITY_INSERT Person.PhoneNumberType ON;
-                INSERT INTO Person.PhoneNumberType
-                    ([PhoneNumberTypeID], [Name], [ModifiedDate])
-                SELECT 
-                    [PhoneNumberTypeID], [Name], [ModifiedDate]
-                FROM AdventureWorks2019.Person.PhoneNumberType;
-            SET IDENTITY_INSERT Person.PhoneNumberType OFF;
-
-            -- Person.CountryRegion
-            INSERT INTO Person.CountryRegion
-                ([CountryRegionCode], [Name], [ModifiedDate])
-            SELECT 
-                [CountryRegionCode], [Name], [ModifiedDate]
-            FROM AdventureWorks2019.Person.CountryRegion;
-
-            -- HumanResources.Shift
-            SET IDENTITY_INSERT HumanResources.Shift ON;
-                INSERT INTO HumanResources.Shift
-                    ([ShiftID], [Name] ,[StartTime], [EndTime], [ModifiedDate])
-                SELECT 
-                    [ShiftID], [Name] ,[StartTime], [EndTime], [ModifiedDate]
-                FROM AdventureWorks2019.HumanResources.Shift;
-            SET IDENTITY_INSERT HumanResources.Shift OFF;
-
-            -- HumanResources.Department
-            SET IDENTITY_INSERT HumanResources.Department ON;
-                INSERT INTO HumanResources.Department
-                    ([DepartmentID], [Name] ,[GroupName], [ModifiedDate])
-                SELECT 
-                    [DepartmentID], [Name] ,[GroupName], [ModifiedDate]
-                FROM AdventureWorks2019.HumanResources.Department;
-            SET IDENTITY_INSERT Agents.Department OFF;
 
             -- Person.BusinessEntity
             SET IDENTITY_INSERT Person.BusinessEntity ON;
@@ -100,26 +36,8 @@ BEGIN
                 SELECT 
                     [BusinessEntityID], [rowguid], [ModifiedDate]
                 FROM AdventureWorks2019.Person.BusinessEntity be
-                WHERE be.BusinessEntityID IN (SELECT BusinessEntityID FROM HumanResources.Employee);
+                WHERE be.BusinessEntityID IN (SELECT BusinessEntityID FROM AdventureWorks2019.HumanResources.Employee);
             SET IDENTITY_INSERT Person.BusinessEntity OFF;
-
-            -- Sales.SalesTerritory
-            SET IDENTITY_INSERT Sales.SalesTerritory ON;
-                INSERT INTO Sales.SalesTerritory
-                    ([TerritoryID], [Name], [CountryRegionCode], [Group], [SalesYTD], [SalesLastYear], [CostYTD], [CostLastYear], [rowguid], [ModifiedDate])
-                SELECT 
-                    [TerritoryID], [Name], [CountryRegionCode], [Group], [SalesYTD], [SalesLastYear], [CostYTD], [CostLastYear], [rowguid], [ModifiedDate]
-                FROM AdventureWorks2019.Sales.SalesTerritory;
-            SET IDENTITY_INSERT Sales.SalesTerritory OFF;
-
-            -- Person.StateProvince
-            SET IDENTITY_INSERT Person.StateProvince ON;
-                INSERT INTO Person.StateProvince
-                    ([StateProvinceID], [StateProvinceCode], [CountryRegionCode], [IsOnlyStateProvinceFlag], [Name], [TerritoryID], [rowguid], [ModifiedDate])
-                SELECT 
-                    [StateProvinceID], [StateProvinceCode], [CountryRegionCode], [IsOnlyStateProvinceFlag], [Name], [TerritoryID], [rowguid], [ModifiedDate]
-                FROM AdventureWorks2019.Person.StateProvince;
-            SET IDENTITY_INSERT Person.StateProvince OFF;
 
             -- Person.Person
             INSERT INTO Person.Person
@@ -127,7 +45,7 @@ BEGIN
             SELECT 
                 [BusinessEntityID], [PersonType], [NameStyle], [Title], [FirstName], [MiddleName], [LastName], [Suffix], [EmailPromotion], [rowguid], [ModifiedDate]
             FROM AdventureWorks2019.Person.Person p
-            WHERE p.BusinessEntityID IN (SELECT BusinessEntityID FROM HumanResources.Employee);
+            WHERE p.BusinessEntityID IN (SELECT BusinessEntityID FROM AdventureWorks2019.HumanResources.Employee);
 
             -- Person.BusinessEntityContact
             INSERT INTO Person.BusinessEntityContact
@@ -135,7 +53,7 @@ BEGIN
             SELECT 
                 [BusinessEntityID], [PersonID], [ContactTypeID], [rowguid], [ModifiedDate]
             FROM AdventureWorks2019.Person.BusinessEntityContact p
-            WHERE p.BusinessEntityID IN (SELECT BusinessEntityID FROM HumanResources.Employee);
+            WHERE p.BusinessEntityID IN (SELECT BusinessEntityID FROM AdventureWorks2019.HumanResources.Employee);
 
             -- Person.EmailAddress
             SET IDENTITY_INSERT Person.EmailAddress ON;
@@ -144,7 +62,7 @@ BEGIN
                 SELECT 
                     [BusinessEntityID], [EmailAddressID], [EmailAddress], [rowguid], [ModifiedDate]
                 FROM AdventureWorks2019.Person.EmailAddress p
-                WHERE p.BusinessEntityID IN (SELECT BusinessEntityID FROM HumanResources.Employee);
+                WHERE p.BusinessEntityID IN (SELECT BusinessEntityID FROM AdventureWorks2019.HumanResources.Employee);
             SET IDENTITY_INSERT Person.EmailAddress OFF;
 
             -- Person.[Password]
@@ -153,7 +71,7 @@ BEGIN
             SELECT 
                 [BusinessEntityID], [PasswordHash], [PasswordSalt], [rowguid], [ModifiedDate]
             FROM AdventureWorks2019.Person.[Password] p
-            WHERE p.BusinessEntityID IN (SELECT BusinessEntityID FROM HumanResources.Employee);
+            WHERE p.BusinessEntityID IN (SELECT BusinessEntityID FROM AdventureWorks2019.HumanResources.Employee);
 
             -- Person.PersonPhone
             INSERT INTO Person.PersonPhone
@@ -161,7 +79,7 @@ BEGIN
             SELECT 
                 [BusinessEntityID], [PhoneNumber], [PhoneNumberTypeID], [ModifiedDate]
             FROM AdventureWorks2019.Person.PersonPhone p
-            WHERE p.BusinessEntityID IN (SELECT BusinessEntityID FROM HumanResources.Employee);
+            WHERE p.BusinessEntityID IN (SELECT BusinessEntityID FROM AdventureWorks2019.HumanResources.Employee);
 
             -- Person.Address
             SET IDENTITY_INSERT Person.Address ON;
@@ -173,8 +91,8 @@ BEGIN
                 WHERE p.AddressID IN 
                     (
                         SELECT AddressID 
-                        FROM Person.BusinessEntityAddress 
-                        WHERE BusinessEntityID IN (SELECT BusinessEntityID FROM HumanResources.Employee)
+                        FROM AdventureWorks2019.Person.BusinessEntityAddress 
+                        WHERE BusinessEntityID IN (SELECT BusinessEntityID FROM AdventureWorks2019.HumanResources.Employee)
                     );
             SET IDENTITY_INSERT Person.Address OFF;
 
@@ -184,7 +102,7 @@ BEGIN
             SELECT 
                 [BusinessEntityID], [AddressID], [AddressTypeID], [rowguid], [ModifiedDate]
             FROM AdventureWorks2019.Person.[BusinessEntityAddress] p
-            WHERE p.BusinessEntityID IN (SELECT BusinessEntityID FROM HumanResources.Employee);
+            WHERE p.BusinessEntityID IN (SELECT BusinessEntityID FROM AdventureWorks2019.HumanResources.Employee);
 
             -- HumanResources.Employee
             INSERT INTO HumanResources.Employee
